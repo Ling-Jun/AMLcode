95663e5bea31040b4af81f928fc1be59d134928f
Bala <bala_b@hotmail.com>
2021-04-26 12:16:55 -0500

    update

--- Designer/TitanicDesigner/agent_dependicy.sh
+++ Designer/TitanicDesigner/agent_dependicy.sh
@@ -1,5 +1,6 @@
 python --version
 pip install azure-cli==2.0.72
+pip install azureml-core
 pip install --upgrade azureml-sdk
 pip install azureml-sdk[notebooks]
 pip install --upgrade azureml-sdk[cli]
