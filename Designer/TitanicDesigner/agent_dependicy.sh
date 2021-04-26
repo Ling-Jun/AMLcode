622694b213548a30e538ed9ca4c48ae1e35dc087
Bala <bala_b@hotmail.com>
2021-04-26 13:05:26 -0500

    upgrade

--- Designer/TitanicDesigner/agent_dependicy.sh
+++ Designer/TitanicDesigner/agent_dependicy.sh
@@ -3,6 +3,7 @@ pip install azure-cli
 pip install azureml-core
 pip install azureml-sdk
 pip install azureml-sdk[notebooks]
+pip install azure-ml-component[notebooks]
 pip install azure-ml-component[notebooks] --extra-index-url https://azuremlsdktestpypi.azureedge.net/modulesdkpreview --upgrade
 pip install --upgrade azureml-sdk[automl]
 pip install --upgrade azureml-sdk[cli]
